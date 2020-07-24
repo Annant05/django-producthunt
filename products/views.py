ba5f11d7d49bafc36958ebd404b48cdebd239ad4
annant05 <annantguptaneema@gmail.com>
2020-07-25 04:20:11 +0530

    complted homepage with polishing

--- products/views.py
+++ products/views.py
@@ -7,10 +7,11 @@ from .models import Product
 # Create your views here.
 
 def home(request):
-    return render(request, 'products/home.html')
+    products = Product.objects
+    return render(request, 'products/home.html', {'products': products})
 
 
-@login_required
+@login_required(login_url="/accounts/signup")
 def create(request):
     if request.method == 'POST':
         if (request.POST['title'] and request.POST['body'] and
@@ -42,7 +43,8 @@ def detail(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
     return render(request, 'products/detail.html', {'product': product})
 
-@login_required
+
+@login_required(login_url="/accounts/signup")
 def upvote(request, product_id):
     if request.method == 'POST':
         product = get_object_or_404(Product, pk=product_id)
