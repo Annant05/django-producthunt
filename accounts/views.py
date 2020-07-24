015292d8ea1d9a6fc8b72486910b66db40067612
annant05 <annantguptaneema@gmail.com>
2020-07-25 01:36:18 +0530

    added authentication logic in accounts module

--- accounts/views.py
+++ accounts/views.py
@@ -20,15 +20,24 @@ def signup(request):
         else:
             return render(request, 'accounts/signup.html', {'error': 'Password Must Match'})
 
-
     else:
         # render the html
         return render(request, 'accounts/signup.html')
 
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        user = auth.authenticate(username=request.POST['username'], password=request.POST['password1'])
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'accounts/login.html', {'error': "Username or password is incorrect."})
+    else:
+        return render(request, 'accounts/login.html')
 
 
 def logout(request):
-    return render(request, 'accounts/signup.html')
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
